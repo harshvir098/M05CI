71ad1e9eb789578d06d1bf56e7bb4ab0b06fc024
34612 <harshvirsingh505@gmail.vom>
2023-02-06 20:00:56 +0100

    jdk 17

---
+++ src/test/java/MatrixTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+class MatrixTest {
+
+    @org.junit.jupiter.api.Test
+    void getWidth() {
+    }
+    @ParameterizedTest
+    @CsvSource({"5,5", "10,20", "9384,12384"})
+    void getWidth_OK(int width, int height) {
+        Matrix m = new Matrix(width, height);
+        Assertions.assertEquals(width, m.getWidth());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getHeight() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fill() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void count() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void found() {
+    }
+
+
+    @org.junit.jupiter.api.Test
+    public
+    String toString() {
+        return null;
+    }
+}
